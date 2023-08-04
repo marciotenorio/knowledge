d66bf193a29ca14a2333f0f7a35b4332780044cd
Márcio Tenório <marciotenorioj@gmail.com>
2023-08-04 00:25:44 -0300

    foreach c#

---
+++ languages/csharp/ConsoleCsharp/ConsoleCsharp/Basics/ForEach.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleCsharp.Basics
+{
+    public class ForEach
+    {
+        //foreach don't let you modify elements because in keyword
+        //if you want modify you should use normal for
+
+        public ForEach()
+        {
+            int[,] matrix =
+            {
+                {1,2,3 },
+                {4,5,6 }
+            };
+
+            foreach (var e in matrix)
+            {
+                Console.Write($"{e} ");
+            }
+        }
+    }
+}
