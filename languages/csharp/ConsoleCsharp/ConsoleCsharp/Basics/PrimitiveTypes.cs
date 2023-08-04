816c2913581986361b9291723fe0f53c4a3e1f68
Márcio Tenório <marciotenorioj@gmail.com>
2023-08-03 23:58:22 -0300

    update structure folder

---
+++ languages/csharp/ConsoleCsharp/ConsoleCsharp/Basics/PrimitiveTypes.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleCsharp.Basics
+{
+    public class PrimitiveTypes
+    {
+        public PrimitiveTypes()
+        {
+            int a = default;
+            long l = a;
+            a = (int)l;
+            float b = 1.3487F;
+            double c = 10.5D;
+            string d;
+            bool e;
+            char f;
+
+            System.Int64 ka = 2, some = 3, somemore = 4;
+
+            Console.WriteLine($"The age is {a}");
+            Console.WriteLine($"Learning print floating points numbers: {b:0.###}");
+        }
+    }
+}
--- languages/csharp/ConsoleCsharp/ConsoleCsharp/Program.cs
+++ languages/csharp/ConsoleCsharp/ConsoleCsharp/Program.cs
@@ -1,5 +1,6 @@
 ﻿
 
+using ConsoleCsharp.Basics;
 using ConsoleCsharp.Service;
 using ConsoleCsharp.Topics;
 using System.Numerics;
@@ -8,26 +9,16 @@ public class Program
 {
 	static void Main(string[] args)
 	{
-		int a = default;
-		long l = a;
-		a = (int)l;
-		float b = 1.3487F;
-		double c = 10.5D;
-		string d;
-		bool e;
-		char f;
-
-		//System.Int64 ka = 2, some = 3, someMore = 4;
-
-  //      Console.WriteLine($"The age is {a}");
-		//Console.WriteLine($"Learning print floating points numbers: {b:0.###}");
 		//FloatPointPrecisionPrint fp = new FloatPointPrecisionPrint();
 		//Console.WriteLine(int.MinValue);
 		//Strings strs = new Strings();
 		//Bools bools = new Bools();
 
-        DefaultParameters defaultParameters = new DefaultParameters();
-        defaultParameters.DefaultParametersExample();
-
+		//DefaultParameters defaultParameters = new DefaultParameters();
+		//defaultParameters.DefaultParametersExample();
+		Arrays arrs = new Arrays();
+		int[] arr = {1,2,3, 4, 5};
+		arrs.myFunc(arr);
+		Console.WriteLine(arr[0]);
     }
 }
