de80e4f0e689c50708ff53cc5466830ec082c4f4
Márcio Tenório <marciotenorioj@gmail.com>
2024-05-28 23:47:52 -0300

    chore: Setups para prática de Java (spring e console).

---
+++ setups/spring-study/src/main/java/com/learn/springstudy/helloworld/HelloWorld.java
@@ -0,0 +1,50 @@
+package com.learn.springstudy.helloworld;
+
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "hello_world")
+public class HelloWorld {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        HelloWorld that = (HelloWorld) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+
+    @Override
+    public String toString() {
+        return STR."HelloWorld{id=\{id}, name='\{name}\{'\''}\{'}'}";
+    }
+}
