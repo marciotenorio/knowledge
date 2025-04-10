07b2bee068964a64472031e85f7581fd7650c256
Márcio Tenório <marciotenorioj@gmail.com>
2025-04-10 00:18:02 -0300

    cp: recursion almost done, have error in assert 195?

---
+++ competitions/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static int count = 0;
+    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        var r = scan.nextLine();
+        System.out.println(r);
+    }
+}
