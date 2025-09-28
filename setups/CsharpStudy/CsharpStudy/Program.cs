8670117d6971c49af3fec5b6bb83d5fd7e42f159
Márcio Tenório <marciotenorioj@gmail.com>
2025-09-28 19:37:49 -0300

    setups: correct setup.

--- setups/CsharpStudy/CsharpStudy/Program.cs
+++ setups/CsharpStudy/CsharpStudy/Program.cs
@@ -1,15 +1,1 @@
-﻿using System.Diagnostics;
-
-var t = new Thread(o =>
-{
-    Thread.Sleep(3_000);
-    Console.WriteLine(Environment.CurrentManagedThreadId);
-});
-
-var sw = new Stopwatch();
-sw.Start();
-
-t.Start();
-
-sw.Stop();
-Console.WriteLine(sw.ElapsedMilliseconds);
+﻿System.Console.WriteLine("Hello World =)");
